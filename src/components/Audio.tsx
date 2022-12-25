b083d4f3a1de6d421c143d32c7e45284fb4285f4
Red Ochsenbein <red@redochsenbein.ch>
2022-12-25 19:16:42 +0100

    Finish spectrum visualization

--- src/components/Audio.tsx
+++ src/components/Audio.tsx
@@ -3,9 +3,11 @@ import React, { useCallback, useEffect, useRef, useState } from 'react'
 const AudioComponent: React.FC = () => {
   const [isPlaying, setIsPlaying] = useState(false)
 
-  const audioRef = useRef<HTMLAudioElement>(null)
-  const audioCtxRef = useRef<AudioContext>()
   const analyserRef = useRef<AnalyserNode>()
+  const audioCtxRef = useRef<AudioContext>()
+  const audioRef = useRef<HTMLAudioElement>(null)
+  const canvasCtxRef = useRef<CanvasRenderingContext2D>()
+  const canvasRef = useRef<HTMLCanvasElement>(null)
   const sourceRef = useRef<MediaElementAudioSourceNode>()
 
   const handleClick = useCallback(() => {
@@ -24,35 +26,58 @@ const AudioComponent: React.FC = () => {
     if (!audioCtxRef.current) {
       audioCtxRef.current = new AudioContext()
       analyserRef.current = audioCtxRef.current.createAnalyser()
-      analyserRef.current.fftSize = 2048
+      analyserRef.current.fftSize = 1024
       sourceRef.current = audioCtxRef.current.createMediaElementSource(audioRef.current)
       sourceRef.current.connect(audioCtxRef.current.destination)
       sourceRef.current.connect(analyserRef.current)
+      canvasCtxRef.current = canvasRef.current?.getContext('2d') ?? undefined
     }
 
     if (!analyserRef.current) {
       return
     }
 
-    const interval = setInterval(() => {
-      if (!analyserRef.current) {
+    const draw = () => {
+      if (!analyserRef.current || !canvasRef.current || !canvasCtxRef.current) {
         return
       }
+
+      const { width, height } = canvasRef.current
 
       const bufferLength = analyserRef.current.frequencyBinCount
       const dataArray = new Uint8Array(bufferLength)
       analyserRef.current.getByteFrequencyData(dataArray)
 
-      console.log(dataArray)
-    }, 1000)
+      canvasCtxRef.current.clearRect(0, 0, width, height)
+      canvasCtxRef.current.fillStyle = 'rgb(0, 0, 0)'
+      canvasCtxRef.current.fillRect(0, 0, width, height)
 
-    return () => clearInterval(interval)
+      const barWidth = (width / bufferLength) * 2.5
+      let barHeight
+      let x = 0
+
+      for (let i = 0; i < bufferLength; i++) {
+        barHeight = (dataArray[i] * height) / 255
+
+        canvasCtxRef.current.fillStyle = `rgb(${dataArray[i]}, 50, 50)`
+        canvasCtxRef.current.fillRect(x, height - barHeight, barWidth, barHeight)
+
+        x += barWidth + 1
+      }
+
+      requestAnimationFrame(draw)
+    }
+
+    draw()
   }, [isPlaying])
 
   return (
     <>
-      <audio src="/sound.mp3" ref={audioRef} loop />
-      <button onClick={handleClick}>{isPlaying ? 'Pause' : 'Play'}</button>
+      <canvas ref={canvasRef} width={1024} height={300}></canvas>
+      <div className="vizWrapper">
+        <audio src="/sound.mp3" ref={audioRef} loop />
+        <button onClick={handleClick}>{isPlaying ? 'Pause' : 'Play'}</button>
+      </div>
     </>
   )
 }
