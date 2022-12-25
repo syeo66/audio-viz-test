19d201aea6379236ac679572f45940be12c8a4bb
Red Ochsenbein <red@redochsenbein.ch>
2022-12-25 15:30:33 +0100

    Basic audio component

--- src/components/Audio.tsx
+++ src/components/Audio.tsx
@@ -1,1 +1,24 @@
-import React from 'react'
+import React, { useCallback, useRef, useState } from 'react'
+
+const AudioComponent: React.FC = () => {
+  const [isPlaying, setIsPlaying] = useState(false)
+
+  const audioRef = useRef<HTMLAudioElement>(null)
+
+  const handleClick = useCallback(() => {
+    setIsPlaying((prev) => {
+      prev ? audioRef.current?.pause() : audioRef.current?.play()
+
+      return !prev
+    })
+  }, [])
+
+  return (
+    <>
+      <audio src="/sound.mp3" ref={audioRef} loop />
+      <button onClick={handleClick}>{isPlaying ? 'Pause' : 'Play'}</button>
+    </>
+  )
+}
+
+export default AudioComponent
