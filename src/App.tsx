9c5427231972edc40692d302316fdd429cd5a527
Red Ochsenbein <red@redochsenbein.ch>
2022-12-25 16:18:53 +0100

    Add analyzer data

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 
 import Audio from './components/Audio'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
       <Audio />
--- src/components/Audio.tsx
+++ src/components/Audio.tsx
@@ -1,9 +1,12 @@
-import React, { useCallback, useRef, useState } from 'react'
+import React, { useCallback, useEffect, useRef, useState } from 'react'
 
 const AudioComponent: React.FC = () => {
   const [isPlaying, setIsPlaying] = useState(false)
 
   const audioRef = useRef<HTMLAudioElement>(null)
+  const audioCtxRef = useRef<AudioContext>()
+  const analyserRef = useRef<AnalyserNode>()
+  const sourceRef = useRef<MediaElementAudioSourceNode>()
 
   const handleClick = useCallback(() => {
     setIsPlaying((prev) => {
@@ -12,6 +15,39 @@ const AudioComponent: React.FC = () => {
       return !prev
     })
   }, [])
+
+  useEffect(() => {
+    if (!audioRef.current || !isPlaying) {
+      return
+    }
+
+    if (!audioCtxRef.current) {
+      audioCtxRef.current = new AudioContext()
+      analyserRef.current = audioCtxRef.current.createAnalyser()
+      analyserRef.current.fftSize = 2048
+      sourceRef.current = audioCtxRef.current.createMediaElementSource(audioRef.current)
+      sourceRef.current.connect(audioCtxRef.current.destination)
+      sourceRef.current.connect(analyserRef.current)
+    }
+
+    if (!analyserRef.current) {
+      return
+    }
+
+    const interval = setInterval(() => {
+      if (!analyserRef.current) {
+        return
+      }
+
+      const bufferLength = analyserRef.current.frequencyBinCount
+      const dataArray = new Uint8Array(bufferLength)
+      analyserRef.current.getByteFrequencyData(dataArray)
+
+      console.log(dataArray)
+    }, 1000)
+
+    return () => clearInterval(interval)
+  }, [isPlaying])
 
   return (
     <>
